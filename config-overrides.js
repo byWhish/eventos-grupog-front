0a34f95142138a844700104f2cebbd5b05720c28
Gerardo Caceres <gpcaceres@teco.com.ar>
2019-04-18 12:15:01 -0300

    chore(): init setup to support decorators

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+const { addDecoratorsLegacy, override, useBabelRc } = require("customize-cra");
+
+/* config-overrides.js */
+module.exports = override(
+    addDecoratorsLegacy(),
+    useBabelRc()
+);
